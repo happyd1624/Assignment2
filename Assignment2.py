78f51a6c809f90e270e1b987f07245cd3ce821fa
happyd1624 <happyd1624@gmail.com>
2026-01-12 22:28:35 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,19 @@
+#Task1 
+
+n = int(input("Enter a Number: "))
+
+if n%2 == 0:
+    print(f"{n} is an even number.")
+
+else:
+    print(f"{n} is an odd number.")
+
+
+
+#Task2
+n = 50
+sum1 = 0
+for i in range(1,51):
+    sum1+= i
+
+print(sum1)
